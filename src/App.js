57e0bf3aeed151f6183de865fa40d7315aa47d12
Hung Vo <vohungbk@gmail.com>
2022-04-12 19:05:23 +0700

    update function zoom

--- src/App.js
+++ src/App.js
@@ -4,33 +4,30 @@ import styled from "styled-components";
 
 import "./App.css";
 import { Page } from "./Component/Page";
-import { Button } from "./Component/Button";
-import { Spacer } from "./Component/Spacer";
 
 function App() {
   const [value, setValue] = React.useState(10);
+  const [isChanged, setIsChanged] = React.useState(false);
 
   const StyledApp = styled.div`
-    width: 100%;
-    height: 100%;
-    min-width: 100%;
-    min-height: 100%;
     transform: scale(${(props) => props.zoom / 100});
-    transform-origin: top;
+    transform-origin: center;
     background: #eeeeee;
   `;
 
   const handleChange = (_, newValue) => {
     setValue(newValue);
+    setIsChanged(true);
   };
 
   return (
-    <div className="App">
-      <StyledApp zoom={value}>
-        <Page zoom={value} />
-        <Spacer size={150} />
-        <Button zoom={value} />
-      </StyledApp>
+    <div className="App" id="App">
+      <div className="content">
+        <StyledApp zoom={value}>
+          <Page zoom={value} isChanged={isChanged} />
+        </StyledApp>
+      </div>
+
       <div className="slider">
         <p className="value">{value}%</p>
         <Slider aria-label="Volume" value={value} onChange={handleChange} max={500} min={10} />
--- src/Component/Page/index.js
+++ src/Component/Page/index.js
@@ -1,24 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stage, Layer, Rect } from "react-konva";
 import styled from "styled-components";
 
 const StyledPage = styled.div`
   background: #fff;
   position: relative;
-  /* transform: scale(${(props) => props.zoom}); */
   transform-origin: top;
 `;
 const MAIN_BACKGROUND_LAYER = {
-  left: 0,
-  bottom: 3300,
-  right: 2550,
   opacity: 255,
   hidden: false,
   name: "Main Background",
   id: 1952,
   type: "layer",
-  height: 2194,
-  width: 2550,
+  width: 500,
 };
 
 export const Page = (props) => {
@@ -31,7 +26,7 @@ export const Page = (props) => {
 
   return (
     <StyledPage zoom={zoom}>
-      <Stage width={2550} height={3300} ref={stageRef}>
+      <Stage width={800} height={800} ref={stageRef}>
         <Layer>
           <Rect
             x={MAIN_BACKGROUND_LAYER.left}
